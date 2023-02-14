a87042df98b9a5719de9f5b0c0b1dd01328057f9
Mouhamadou-Soumare <82391111+Mouhamadou-Soumare@users.noreply.github.com>
2023-02-14 16:07:58 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/login/login';
+import Register from './Components/register/register';
+import Nav from './Components/nav/nav';
 
 function App() {
   return (
     <div className="App">
       <Login/>
-      
+      <Register/>
+      <section></section>
+      <Nav/>
+
+
     </div>
   );
 }
--- src/Components/login/login.js
+++ src/Components/login/login.js
@@ -1,47 +1,71 @@
-import React from 'react'
-
+import React from "react";
 
 const Login = () => {
+  return (
+    <section id="signIn">
+      <div className="container">
+        <div className="container-inside">
+          <div className="titre">
+            <h1>Sign In</h1>
+            <p>
+              You don’t have an account yet ?{" "}
+              <span className="link-color-primary">
+                <a> Sign Up</a>
+              </span>
+            </p>
+          </div>
 
-
-    return (
-        <section id='signIn'>
-            <div className='container'>
-                <div className='container-inside'>
-                    <div className='titre'>
-                        <h1>Sign In</h1>
-                        <p>You don’t have an account yet ?</p>
-                    </div>
-
-                    <form class="bg-white  rounded px-8 pt-6 pb-8 mb-4">
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2 label-form" for="username">
-        Username
-      </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
-    </div>
-    <div class="mb-6">
-      <label className="block text-gray-700 text-sm font-bold mb-2 label-form" for="password">
-        Password
-      </label>
-      <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-      <p className="text-red-500 text-xs italic">Please choose a password.</p>
-      <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
-      </a>
-    </div>
-    <div className="flex items-center justify-center">
-      <button className="text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline button-form" type="button">
-        Sign In
-      </button>
-      
-    </div>
-  </form>
-                   
-                </div> 
+          <form class="bg-white  rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2 label-form"
+                for="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Enter your email"
+              />
             </div>
-        </section>
-    )
-}
+            <div class="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2 label-form"
+                for="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="******************"
+              />
+              <p className="float-left">
+                {" "}
+                <a
+                  className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                  href="#"
+                >
+                  Forgot Password?
+                </a>
+              </p>
+            </div>
+            <div className="flex items-center justify-center">
+              <button
+                className="text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline button-form"
+                type="button"
+              >
+                Log In
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Login
+export default Login;
