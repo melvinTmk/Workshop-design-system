da80d60fc44865d04adb38b970683c24685181db
Mouhamadou-Soumare <82391111+Mouhamadou-Soumare@users.noreply.github.com>
2023-02-14 01:26:57 +0100

    maj

---
+++ src/Components/login/login.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const Login = () => {
+
+
+    return (
+        <section id='signIn'>
+            <div className='container'>
+                <div className='container-inside'>
+                    <div className='titre'>
+                        <h5>Titre</h5>
+                        <p>sous titre</p>
+                    </div>
+
+                    <div className='input'>
+
+                    </div>
+                </div> 
+            </div>
+        </section>
+    )
+}
+
+export default Login
